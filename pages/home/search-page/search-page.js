d8bd7bf0881df38980268fd979c28f296114e007
chenyongduan <250881711@qq.com>
2017-11-27 22:23:22 +0800

    根据评价类型查询结果逻辑；

--- pages/home/search-page/search-page.js
+++ pages/home/search-page/search-page.js
@@ -6,6 +6,7 @@ Page({
     curStudentInfo: {},
     searchOptions: [],
     commentOptions: [],
+    searchResult: [],
   },
   studentInfos: [],
   inputValue: '',
@@ -23,7 +24,6 @@ Page({
           } catch (e) {}
         });
       });
-      console.error(studentInfos)
       this.studentInfos = studentInfos;
     });
     wxPromise(wx.getStorage, { key: 'comment' }).then((res) => {
@@ -36,15 +36,64 @@ Page({
   },
   bindSearchInput: function (e) {
     this.inputValue = e.detail.value;
+    const { commentOptions } = this.data;
+    const searchOptions = [];
+    commentOptions.map((value) => {
+      const isSame = value.name.match(this.inputValue);
+      if (isSame && this.inputValue !== '') {
+        searchOptions.push(value);
+      }
+    });
+    this.setData({ searchOptions });
+  },
+  matchByCommentName: function () {
+    const { commentOptions } = this.data;
+    let commentId = null;
+    commentOptions.map((value) => {
+      const isSame = value.name.search(this.inputValue);
+      if (isSame === 0) {
+        commentId = value.id;
+      }
+    });
+    return commentId;
   },
   onSearchClick: function (e) {
-    if (this.inputValue === '') return;
-    for (let i = 0; i < this.studentInfos.length; i += 1) {
-      if (this.studentInfos[i].name === this.inputValue) {
-        console.warn(this.studentInfos[i])
-        this.setData({ curStudentInfo: this.studentInfos[i] });
-        break;
-      }
+    if (this.inputValue === '') {
+      wx.showToast({
+        title: '评价类型为空!',
+      });
+      return;
     }
+    const commentId = this.matchByCommentName();
+    if (commentId) {
+      this.getSearchByCommentId(commentId);
+    }
+  },
+  onSearchItemClick: function (e) {
+    const { id } = e.currentTarget.dataset;
+    this.getSearchByCommentId(id);
+  },
+  getSearchByCommentId: function (id) {
+    const searchResult = [];
+    this.studentInfos.map((studentInfo) => {
+      const commentInfo = studentInfo.comment || [];
+      const commentKeys = Object.keys(commentInfo);
+      const searchInfo = [];
+      commentKeys.map((commentKey) => {
+        commentInfo[commentKey].map((commentId) => {
+          if (Number(commentId) === id) {
+            searchInfo.push(commentKey);
+          }
+        })
+      });
+      if (searchInfo.length > 0) {
+        const curStudentInfo = {};
+        curStudentInfo.id = studentInfo.id;
+        curStudentInfo.name = studentInfo.name;
+        curStudentInfo.commentKeys = searchInfo;
+        searchResult.push(curStudentInfo);
+      }
+    });
+    this.setData({ searchResult, searchOptions: [] });
   }
 })
